c18351ea91586f29b8062f615f22359f7dc1883b
ghassanabboud <ghassan.abboud@epfl.ch>
2026-03-06 22:08:15 +0100

    typo

--- RCP/example_runs.sh
+++ RCP/example_runs.sh
@@ -2,7 +2,7 @@
 # Use these first to verify that you can run the models, and then modify them to your needs.
 
 # First, a template for opening a container with the model of your choice. 
-# Fill in your gaspar username and job name then choose the correct model iamage.
+# Fill in your gaspar username and job name then choose the correct model image before running the corresponding test command from the examples below.
 
 runai submit <your_job_name> \
     --project hackathon-proteindesign-<gaspar_username> \
@@ -53,6 +53,10 @@ carbonara \
 boltz predict /mnt/shared-ro/examples/boltz/prot_no_msa.yaml --out_dir /mnt/scratch/boltz_example/
 
 #chai example: -i registry.rcp.epfl.ch/proteindesign-containers/chai:2026.1
+# for more details on configuring runs, run "chai-lab --help" inside the container
+# or consult official repository README: https://github.com/chaidiscovery/chai-lab
+# with chai installed you can also run jobs from a python script like in the repo's examples: https://github.com/chaidiscovery/chai-lab/tree/main/examples
 
+chai-lab fold /mnt/shared-ro/examples/chai/8cyo.fasta /mnt/scratch/example-ghassan-chai --constraint-path /mnt/shared-ro/examples/chai/8cyo.restraints 
 
-
+#af3 example: -i registry.rcp.epfl.ch/proteindesign-containers/af3:2026.1
